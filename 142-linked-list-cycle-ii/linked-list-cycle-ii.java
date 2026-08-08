535581349e53c765be8f15e455a2643798350b88
Sunnyykadam <147860651+Sunnyykadam@users.noreply.github.com>
2026-08-08 21:36:15 +0530

    Time: 1 ms (19.00%) | Memory: 46.3 MB (97.03%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -10,52 +10,35 @@
  * }
  */
 public class Solution {
-         public int lengthCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) {
-                // calculate the length
-                ListNode temp = slow;
-                int length = 0;
-                do {
-                    temp = temp.next;
-                    length++;
-                } while (temp != slow);
-                return length;
-            }
-        }
-        return 0;
-    }
     public ListNode detectCycle(ListNode head) {
-        int length = 0;
+        int length=0;
         ListNode fast = head;
         ListNode slow = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) {
-                length = lengthCycle(slow);
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                do{
+                    length++;
+                    slow=slow.next;
+                }while(slow!=fast);
                 break;
             }
         }
+        ListNode f = head;
+        ListNode s = head;
         if (length == 0) {
             return null;
         }
-        ListNode f = head;
-        ListNode s = head;
-
-        while (length > 0) {
-            s = s.next;
+        while(length>0){
+            s=s.next;
             length--;
         }
-        while (f != s) {
-            f = f.next;
-            s = s.next;
+        while(f!=s){
+            f=f.next;
+            s=s.next;
         }
-        return s;
+        return f;
+        
     }
 }
